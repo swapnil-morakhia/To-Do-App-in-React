b972e7cc6aa1fd3be39d57fc2965c8d008499140
swapnil24011996 <smorakhi@stevens.edu>
2020-07-19 01:30:57 -0400

    Updates

---
+++ precache-manifest.d9626b526d997d447df4c90c874ccd0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92f72f15880b2b3762ab1b370a71460b",
+    "url": "/To-Do-App-in-React/index.html"
+  },
+  {
+    "revision": "d4cda5995036311c9a41",
+    "url": "/To-Do-App-in-React/static/css/main.031e540d.chunk.css"
+  },
+  {
+    "revision": "5db7f2d4b03ed35435c7",
+    "url": "/To-Do-App-in-React/static/js/2.03b3a3f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-App-in-React/static/js/2.03b3a3f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4cda5995036311c9a41",
+    "url": "/To-Do-App-in-React/static/js/main.61e9386f.chunk.js"
+  },
+  {
+    "revision": "8e4ad4c82cdd4e61ac96",
+    "url": "/To-Do-App-in-React/static/js/runtime-main.da4c7d54.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/To-Do-App-in-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
